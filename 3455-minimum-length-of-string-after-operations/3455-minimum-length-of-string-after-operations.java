c6d595034cddd28c6e8b87a8c5619cba62e4d271
Aanchal3134 <130480155+Aanchal3134@users.noreply.github.com>
2025-01-14 01:42:23 +0530

    Time: 87 ms (30%), Space: 50.8 MB (13.33%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.java
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.java
@@ -1,21 +1,18 @@
 class Solution {
     public int minimumLength(String s) {
-        //use freq array - hashtable - METHOD 1
-        int totalLength = 0;
-        int[] freqArr = new int[26]; ///store freq if letters
-
-        for(char c : s.toCharArray()) {
-            freqArr[c - 'a']++;
+        //METHOD 2 - HASHMAPS
+        HashMap<Character, Integer> count = new HashMap<>();
+        for(char c : s.toCharArray()) { //store the char and their freq in hashmap count
+            count.put(c, count.getOrDefault(c, 0) + 1);
         }
 
-        for(int f : freqArr) {
-            if(f == 0) continue;
-            if(f % 2 == 0) { //if char freq is even then 2 char t=will remain in string
-                totalLength += 2;
-            } else { //odd freq char - 1 char will remain
-                totalLength += 1;
-            }   
-        }     
-        return totalLength;
+        int minus = 0;
+        for(int value : count.values()) { 
+            while(value >= 3) {
+                minus += 2;
+                value -= 2;
+            }
+        }
+        return s.length() - minus;
     }
 }
