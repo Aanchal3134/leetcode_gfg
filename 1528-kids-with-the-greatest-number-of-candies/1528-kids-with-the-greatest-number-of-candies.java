d49bdccdfa1b5c704cf261f9ca896d063c7827c6
Aanchal3134 <130480155+Aanchal3134@users.noreply.github.com>
2025-01-15 20:08:18 +0530

    Time: 0 ms (100%), Space: 42.6 MB (34.93%) - LeetHub

--- 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.java
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.java
@@ -3,10 +3,16 @@ class Solution {
         int n = candies.length;
         List<Boolean> result = new ArrayList<>();
 
-        //find the largest elem in array
-        int larg = largest(candies);
+        int max = Integer.MIN_VALUE;
+
+        for(int i=0; i<n; i++) {
+            if(max < candies[i]) {
+                max = candies[i];
+            }
+        }//find the largest elem in array
+        
         for(int i=0; i<candies.length; i++) {
-            if(candies[i]+extraCandies >= larg) {
+            if(candies[i]+extraCandies >= max) {
                 result.add(true);
             } else {
                 result.add(false);
@@ -16,15 +22,5 @@ class Solution {
         return result;
     }
 
-    public int largest(int[] arr) {
-        int n = arr.length;
-        int max = Integer.MIN_VALUE;
-
-        for(int i=0; i<n; i++) {
-            if(max < arr[i]) {
-                max = arr[i];
-            }
-        }
-        return max;
-    }
+    
 }
