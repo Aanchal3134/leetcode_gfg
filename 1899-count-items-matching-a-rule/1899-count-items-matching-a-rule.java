4b641dbe05ebc726e6b7579fa39f140ed4a93852
Aanchal3134 <130480155+Aanchal3134@users.noreply.github.com>
2025-01-19 02:02:01 +0530

    Time: 3 ms (98.86%), Space: 50.9 MB (32.59%) - LeetHub

---
+++ 1899-count-items-matching-a-rule/1899-count-items-matching-a-rule.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {//GPT GAVE CORRECT APPROACH
+        int count = 0;
+        if(ruleKey.equals("type")) {
+            for(int i=0; i<items.size(); i++) {
+                if(ruleValue.equals(items.get(i).get(0))) {
+                    count++;
+                }
+            }
+        }
+        else if(ruleKey.equals("color")) {
+            for(int i=0; i<items.size(); i++) {
+                if(ruleValue.equals(items.get(i).get(1))) {
+                    count++;
+                }
+            }
+        }else {
+            for(int i=0; i<items.size(); i++) {
+                if(ruleValue.equals(items.get(i).get(2))) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
