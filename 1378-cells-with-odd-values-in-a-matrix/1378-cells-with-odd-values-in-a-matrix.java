f75664a8e7db8c50b2a34113d54b06eb621f93da
Aanchal3134 <130480155+Aanchal3134@users.noreply.github.com>
2025-01-27 02:51:39 +0530

    Time: 0 ms (100%), Space: 41.9 MB (29.08%) - LeetHub

--- 1378-cells-with-odd-values-in-a-matrix/1378-cells-with-odd-values-in-a-matrix.java
+++ 1378-cells-with-odd-values-in-a-matrix/1378-cells-with-odd-values-in-a-matrix.java
@@ -1,28 +1,32 @@
 class Solution {
     public int oddCells(int m, int n, int[][] indices) {
-        //VIDEO SOLUTION
-        int[][] ans = new int[m][n];
+        //VIDEO SOLUTION - OPTIMISED APPROACH
+        boolean[] row = new boolean[m];
+        boolean[] col = new boolean[n];
 
-        int count = 0;
-//m - rows, n- cols
-        for(int i=0; i<indices.length; i++) { //till no of rows in indices
-            for(int j=0; j<n; j++) { //row remains constant , col changes
-                ans[indices[i][0]][j]++;
-            }
-            for(int j=0; j<m; j++) {
-                //col remians same , row changes
-                ans[j][indices[i][1]]++;
-            }
+        for(int i=0; i<indices.length; i++) {
+            row[indices[i][0]] ^= true;
         }
 
-        //COUNT ODD NUMBERS IN ANS
+        for(int i=0; i<indices.length; i++) {
+            col[indices[i][1]] ^= true;
+        }
+
+        //cal r and c
+        int r = 0;
+        int c = 0;
+
         for(int i=0; i<m; i++) {
-            for(int j=0; j<n; j++) {
-                if(ans[i][j] % 2 != 0) {
-                    count++;
-                }
+            if(row[i]) {
+                r++;
             }
         }
-        return count;
+        for(int i=0; i<n; i++) {
+            if(col[i]) {
+                c++;
+            }
+        }
+
+        return (r*n + c*m) - 2*(r*c);
     }
 }
