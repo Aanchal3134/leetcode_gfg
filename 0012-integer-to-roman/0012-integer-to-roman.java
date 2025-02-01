6e09ec646854098798492f541aa9762dde1105d8
Aanchal3134 <130480155+Aanchal3134@users.noreply.github.com>
2025-02-01 20:48:05 +0530

    Time: 3 ms (98.65%), Space: 44.3 MB (83.58%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,58 +1,18 @@
 class Solution {
     public String intToRoman(int num) {
-        Map<Integer, String> map = new HashMap<>();
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
         StringBuilder ans = new StringBuilder();
 
-        map.put(1, "I");
-        map.put(2, "II");
-        map.put(3, "III");
-        map.put(4, "IV");
-        map.put(5, "V");
-        map.put(6, "VI");
-        map.put(7, "VII");
-        map.put(8, "VIII");
-        map.put(9, "IX");
-        map.put(10, "X");
-        map.put(20, "XX");
-        map.put(30, "XXX");
-        map.put(40, "XL");
-        map.put(50, "L");
-        map.put(60, "LX");
-        map.put(70, "LXX");
-        map.put(80, "LXXX");
-        map.put(90, "XC");
-        map.put(100, "C");
-        map.put(200, "CC");
-        map.put(300, "CCC");
-        map.put(400, "CD");
-        map.put(500, "D");
-        map.put(600, "DC");
-        map.put(700, "DCC");
-        map.put(800, "DCCC");
-        map.put(900, "CM");
-        map.put(1000, "M");
-        map.put(2000, "MM");
-        map.put(3000, "MMM");
-        
-
-        int n = 0;
-        int m = 0;
-
-        while(num > 0) {
-            int ld = num % 10;
-            if(ld == 0) {
-                while(ld == 0) {
-                    num = num/10;
-                    ld = num % 10;
-                    n++;
-                }
+        for(int i=0; i<values.length; i++) {
+            if(num == 0) break;
+            while(num >= values[i]) {
+                ans.append(symbols[i]);
+                num -= values[i];
             }
-            m = (int)Math.pow(10, n);        
-            ans.insert(0, map.get(m * ld));
-            n++;
-            num = num / 10;
         }
+
         return ans.toString();
     }
 }
