8f97b3a8cf1bd7ea97c180f15a288e49665622eb
Aanchal3134 <130480155+Aanchal3134@users.noreply.github.com>
2025-02-09 21:24:57 +0530

    Time: 70 ms (56.11%), Space: 57.3 MB (75.26%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,38 +1,22 @@
-class Solution { //BRUTE FORCE
+class Solution { //optimised - using stack
     public int largestRectangleArea(int[] heights) {
         int n = heights.length;
-        int[] left = new int[n];
-        int[] right = new int[n];
+
         int maxArea = 0;
+        Stack<Integer> st = new Stack<>();
 
-        left[0] = -1;
-        right[n-1] = n;
-
-        //left bound
-        for(int i=1; i<n; i++) {
-            int prev = i-1;
-            while(prev >= 0 && heights[prev] >= heights[i]) {
-                prev = left[prev];
+        for(int i=0; i<=n; i++) {
+            int currH = (i == n) ? 0 : heights[i];
+            while(!st.isEmpty() && currH < heights[st.peek()]) {
+                int top = st.pop();
+                int width = (st.isEmpty()) ? i : i - st.peek() - 1;
+                //area - heights[top] * widht
+                maxArea = Math.max(heights[top] * width, maxArea);
             }
-            left[i] = prev;
-        }
-
-        //right boundary
-        for(int i=n-2; i>=0; i--) {
-            int after = i+1;
-            while(after < n && heights[after] >= heights[i]) {
-                after = right[after];
-            }
-            right[i] = after;
-        }
-
-        //find area
-        for(int i=0; i<n; i++) {
-            int width = right[i] - left[i] - 1;
-            int height = heights[i];
-            maxArea = Math.max(maxArea, height * width);
+            st.push(i);
         }
 
         return maxArea;
+
     }
 }
