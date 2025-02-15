01fe52a55d2717df6553b9ee9bffc0c58af03a90
Aanchal3134 <130480155+Aanchal3134@users.noreply.github.com>
2025-02-16 02:09:54 +0530

    Time: 0 ms (100%), Space: 42.1 MB (29.49%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,23 +1,27 @@
 class Solution {
     public void sortColors(int[] nums) {
-        //USING HASHMAPS
-        HashMap<Integer, Integer> count = new HashMap<>();
-        count.put(0, 0);
-        count.put(1, 0);
-        count.put(2, 0);
+        //dutch flag algo - 3 pointers !!
+        int white = 0;
+        int red = 0;
+        int blue = nums.length-1;
 
-        for(int num : nums) {
-            count.put(num, count.get(num)+1);
-        }
-
-        int idx = 0;
-        for(int k=0; k<3; k++) {
-            int freq = count.get(k);
-            for(int j=0; j<freq; j++) {
-                nums[idx] = k;
-                idx++;
+        while(white <= blue) {
+            if(nums[white] == 0) {
+                //swap with red
+                int temp = nums[white];
+                nums[white] = nums[red];
+                nums[red] = temp;
+                red++;
+                white++;
+            } else if(nums[white] == 1) {
+                white++;
+            } else {
+                //nums[white] == 2-> swap with blue
+                int temp = nums[white];
+                nums[white] = nums[blue];
+                nums[blue] = temp;
+                blue--;
             }
         }
-        
     }
 }
