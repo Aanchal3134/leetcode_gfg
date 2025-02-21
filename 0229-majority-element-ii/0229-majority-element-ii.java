eb0bbc50fe328d445f86e97958efd9f26727d3ca
Aanchal3134 <130480155+Aanchal3134@users.noreply.github.com>
2025-02-22 03:26:24 +0530

    Time: 2 ms (99.07%), Space: 48.5 MB (18.52%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,21 +1,40 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
+        //MOOR'S VOTING ALFO - STRIVER'S VIDEO
         int n = nums.length;
-        int target = n/3 + 1;
+        int target = n/3;
         List<Integer> list = new ArrayList<>();
 
-        Map<Integer, Integer> map = new HashMap<>();
-        //find freq of each elem
-        //iterate over hashmap to find if freq of elem is > target - if yes add to list    
+        int count1 = 0, count2 = 0;
+        int el1 = 0, el2 = 0;
+
         for(int i=0; i<n; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
-
-            if(map.get(nums[i]) == target) {
-                list.add(nums[i]);
+            if(count1 == 0 && nums[i] != el2) {
+                el1 = nums[i];
+                count1 = 1;
+            }else if(count2 == 0 && nums[i] != el1) {
+                el2 = nums[i];
+                count2 = 1;
+            } else if(nums[i] == el1) {
+                count1++;
+            } else if(nums[i] == el2) {
+                count2++;
+            } else {
+                count1--;
+                count2--;
             }
-
-            if(list.size() == 2) break;
         }
+        int c1 = 0, c2 = 0;
+        for(int i=0; i<n; i++) {
+            if(el1 == nums[i]) {
+                c1++;
+            }
+            if(el2 == nums[i]) {
+                c2++;
+            }
+        }
+        if(c1 > target) { list.add(el1); } 
+        if(c2 > target && el1 != el2) { list.add(el2); }
         return list;
     }
 }
