962ed1ff9035166206104f20d3bc936471d33330
Aanchal3134 <130480155+Aanchal3134@users.noreply.github.com>
2025-02-22 01:59:59 +0530

    Time: 16 ms (10.94%), Space: 47.7 MB (72.14%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int n = nums.length;
-        int target = n/3;
+        int target = n/3 + 1;
         List<Integer> list = new ArrayList<>();
 
         Map<Integer, Integer> map = new HashMap<>();
@@ -9,12 +9,12 @@ class Solution {
         //iterate over hashmap to find if freq of elem is > target - if yes add to list    
         for(int i=0; i<n; i++) {
             map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
-        }
 
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if(entry.getValue() > target) {
-                list.add(entry.getKey());
+            if(map.get(nums[i]) == target) {
+                list.add(nums[i]);
             }
+
+            if(list.size() == 2) break;
         }
         return list;
     }
